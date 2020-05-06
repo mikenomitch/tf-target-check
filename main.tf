292b5a28d509bd4e416d317a4804eb6033a7cdec
Michael Nomitch <mike@Michaels-MacBook-Pro-2.local>
2020-05-06 15:18:12 -0500

    Change ingress rule to fail

--- main.tf
+++ main.tf
@@ -26,12 +26,12 @@ resource "aws_security_group" "foo_security_group" {
   name        = "public_sg"
   description = "Public group"
 
-  // ingress {
-  //   from_port   = 80
-  //   to_port     = 80
-  //   protocol    = "tcp"
-  //   cidr_blocks = ["0.0.0.0/0"]
-  // }
+  ingress {
+    from_port   = 80
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
     from_port   = 0
