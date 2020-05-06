176451173e93f52894c2a779608501beda5292eb
Michael Nomitch <mike@Michaels-MacBook-Pro-2.local>
2020-05-06 14:08:05 -0500

    Simple setup

---
+++ main.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region     = "${var.aws_region}"
+}
+
+resource "aws_vpc" "foo_vpc" {
+  cidr_block = "10.10.0.0/16"
+}
+
+resource "aws_vpc" "bar_vpc" {
+  cidr_block = "10.10.0.0/16"
+}
+
+resource "aws_s3_bucket" "baz_bucket" {
+  bucket = "baz_bucket"
+  acl = "private"
+}
+
+resource "aws_s3_bucket" "qux_bucket" {
+  bucket = "baz_bucket"
+  acl = "private"
+}
