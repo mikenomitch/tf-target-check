10a074c36337a5197b2695ec7b08d5aa5eb71ffe
Michael Nomitch <mike@Michaels-MacBook-Pro-2.local>
2020-05-06 15:09:39 -0500

    Fix vpc cidr and add seecurity group

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 }
 
 resource "aws_vpc" "foo_vpc" {
-  cidr_block = "0.0.0.0"
+  cidr_block = "10.10.0.0/16"
 }
 
 resource "aws_vpc" "bar_vpc" {
@@ -20,4 +20,23 @@ resource "aws_s3_bucket" "baz_bucket" {
 resource "aws_s3_bucket" "qux_bucket" {
   bucket = "qux"
   acl = "private"
+}
+
+resource "aws_security_group" "foo_security_group" {
+  name        = "public_sg"
+  description = "Public group"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0./0"]
+  }
 }
