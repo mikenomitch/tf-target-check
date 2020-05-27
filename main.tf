7923429b24ba4f8e34ead82801b9a00a6249fe93
Mike Nomitch <mnomitch@hashicorp.com>
2020-05-27 15:55:35 -0500

    Get rid of interpolation only values

--- main.tf
+++ main.tf
@@ -10,9 +10,9 @@ terraform {
 }
 
 provider "aws" {
-  access_key = "${var.aws_access_key}"
-  secret_key = "${var.aws_secret_key}"
-  region     = "${var.aws_region}"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  region     = var.aws_region
 }
 
 resource "aws_vpc" "foo_vpc" {
